2490de2e652ae77a6de9ab7e9b9a445dc180fc15
EfimkaBobr <49053837+EfimkaBobr@users.noreply.github.com>
2020-05-08 00:04:57 +0600

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Snake_Py',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='GNU',
+    author='Efim',
+    author_email='efimkabobr@gmail.com',
+    description='My first snake written in python using pygame library'
+)
